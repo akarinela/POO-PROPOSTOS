f917d5fd36b83949ced794c52d7ffaa67cce5eed
ana karine <ana.karine06@aluno.ifce.edu.br>
2025-03-24 11:19:33 -0300

    Add files via upload

---
+++ Q01/src/br/edu/principal/Principal.java
@@ -0,0 +1,18 @@
+package br.edu.principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		int N1 = 10;
+		int N2 = 15;
+		
+		int soma = N1 - N2;
+		
+		System.out.println("O primeiro número: " + N1);
+		System.out.println("O segundo número: " + N2);
+		System.out.println("O resultado da subtração: " + soma);
+
+	}
+
+}
